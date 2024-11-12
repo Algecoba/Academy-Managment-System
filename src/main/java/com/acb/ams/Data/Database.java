d1c3a8a52ef45dc02bfae4398e8f3d516352236e
Algecoba <corralesbarrera.a@gmail.com>
2024-11-12 17:30:17 -0500

    Hola

---
+++ src/main/java/com/acb/ams/Data/Database.java
@@ -0,0 +1,18 @@
+package com.acb.ams.Data;
+import java.sql.*;
+
+
+public class Database {
+
+    /*Public Connection connectDB(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connect = DriverManager.getConnection("","root","");
+            return connect;
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }*/
+}
